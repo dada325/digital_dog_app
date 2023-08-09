07607a254b352b367913bf605f4e17e77584f063
Wenda Zheng <dylan.d.zheng@gmail.com>
2023-08-09 21:51:15 +0200

    Update digital_dog_app.py

--- digital_dog_app.py
+++ digital_dog_app.py
@@ -1,53 +1,62 @@
+# digital_dog_app.py
+#
+#
+# A base implementation using streamlit 
+#
+
+
+
+
 import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, firestore
+from modules import api, database, cookie_manager, utilities
+import logging
 
-# Firebase Initialization
-cred = credentials.Certificate("path_to_service_account_key.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+# Initialize logging
+logging.basicConfig(filename='logs/app.log', level=logging.INFO, format='%(asctime)s - %(message)s')
+logger = logging.getLogger('digital_dog_app')
 
-# Firestore Functions
-def insert_message(user_id, sender, message):
-    chats_ref = db.collection(u'chats').document(user_id).collection(u'messages')
-    chats_ref.add({
-        'sender': sender,
-        'message': message,
-        'timestamp': firestore.SERVER_TIMESTAMP
-    })
+# Title & Introduction
+st.title("Digital Dog Companion")
 
-def get_chat_history(user_id):
-    chats_ref = db.collection(u'chats').document(user_id).collection(u'messages').order_by('timestamp')
-    chats = chats_ref.stream()
-    
-    chat_history = [{"sender": chat.to_dict()["sender"], "message": chat.to_dict()["message"]} for chat in chats]
-    return chat_history
+# Check if user has a cookie already (this is a pseudo-function, in reality, you'd have to manage cookies differently with Streamlit)
+user_id = cookie_manager.get_cookie()
 
-# Streamlit UI
-st.title("Your Cosmo Doggie")
+if not user_id:
+    user_id = cookie_manager.set_cookie()
+    st.write("Welcome to the Digital Dog Companion! Let's get started.")
+    dog_avatar = utilities.assign_dog_avatar()
+    dog_name = st.text_input("What would you like to name your dog?")
+    if dog_name:
+        # Save user details to the database
+        database.save_user_data(user_id, dog_name, dog_avatar)
+else:
+    user_data = database.get_user_data(user_id)
+    dog_name = user_data['dog_name']
+    dog_avatar = user_data['dog_avatar']
 
-# Display the dog avatar
-dog_avatar = "assets/path_to_dog_avatar.png"  # Replace with your path
-st.image(dog_avatar, caption="Meet Rover, your new Friend!!")
+# Display Dog Avatar and Name
+st.image(dog_avatar, caption=dog_name, use_column_width=True)
 
-# Assuming you have some mechanism to identify users
-user_id = "unique_user_identifier"  # Replace with your user identification mechanism
+# Display previous conversations
+previous_conversations = database.fetch_conversation(user_id)
+for interaction in previous_conversations:
+    st.write(f"You: {interaction['user']}")
+    st.write(f"{dog_name}: {interaction['dog']}")
 
-# Fetch chat history for the user
-chat_history = get_chat_history(user_id)
+# Chat Interface
+user_input = st.text_input("Talk to your dog:")
+if user_input:
+    dog_response = api.get_dog_response(user_input, dog_name)
+    database.save_conversation(user_id, user_input, dog_response)
+    st.write(f"You: {user_input}")
+    st.write(f"{dog_name}: {dog_response}")
 
-# Display chat history
-for chat in chat_history:
-    if chat["sender"] == "User":
-        st.container().write({"User": chat["message"]}, background_color="lightgray")
-    else:
-        st.container().write({"Rover": chat["message"]}, background_color="lightblue")
+    # Optionally collect feedback
+    feedback = st.text_area("Feedback on the response:", "Type feedback here...")
+    if feedback:
+        # Save feedback for analysis
+        database.save_feedback(user_input, dog_response, feedback)
 
-# Input Box and Send Button
-user_input = st.text_input("Type your message here...")
-if st.button("Send"):
-    if user_input:  
-        insert_message(user_id, "User", user_input)
-        # Here, you'd handle sending the message to the backend/AI model
-        # For simplicity, let's add a dummy dog response
-        insert_message(user_id, "Rover", "Woof! I'm thinking...")
+# Footer & Additional Information
+st.write("---")
+st.write("Digital Dog Companion - An Interactive Chatbot")
